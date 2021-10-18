c8d907957f1ea6befed95c7494f6127e31e9cc9f
Mariana Picolo <marianapicolo@hotmail.com>
2021-10-17 22:01:03 -0300

    feat: add structopt to parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "clippings",
+    about = "All the commands available on clippings manager."
+)]
+struct Opt {
+    #[structopt(short, long)]
+    debug: bool,
+
+    // Input file
+    #[structopt(short, long, parse(from_os_str))]
+    file: PathBuf,
+
+    // Exported parsed file format
+    #[structopt(short = "e", long)]
+    export_type: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, clippings!");
+
+    let opt = Opt::from_args();
+    println!("{:#?}", opt);
 }
